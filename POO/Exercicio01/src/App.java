839acf0e8edcca1024489f96be3bfe2f07687785
Djúlia Sabrina Preti <dpreti@furb.br>
2026-09-16 07:58:29 -0300

    Corrigindo diagrama de classes da Vet

---
+++ POO/Exercicio01/src/App.java
@@ -0,0 +1,14 @@
+public class Pessoa {
+    public Pessoa() {
+        String nome;
+        int idade = 0;
+        Float altura = 0;
+        Boolean maior_idade = false;
+    }
+
+    privst
+
+    public static void main(String[] args) throws Exception {
+        System.out.println("Digite se");
+    }
+}
